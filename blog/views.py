5cf331a4b34f2da4edea5e4a512a9322b50e8265
Your Name <you@example.com>
2019-10-27 19:21:41 +0900

    first commit

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now())\
         .order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    Post.objects.get(pk=pk)
