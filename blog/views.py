ba7f9176bd4916baa41736c971260e66bd05729c
Your Name <you@example.com>
2019-10-27 20:20:01 +0900

    first commit

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
-    Post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
