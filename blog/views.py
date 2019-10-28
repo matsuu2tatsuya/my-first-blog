61841da28e50ae7d96ea08480fcf72a53a8637c3
Your Name <you@example.com>
2019-10-28 23:13:35 +0900

    first commit

--- blog/views.py
+++ blog/views.py
@@ -42,5 +42,10 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
+
 
 
