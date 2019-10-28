4016509022e01a9ac267e9eecb4d50199cb14064
Your Name <you@example.com>
2019-10-28 22:53:58 +0900

    first commit

--- blog/views.py
+++ blog/views.py
@@ -22,7 +22,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
