c12000e8a23abd0e727e9a955121c339659b647b
Your Name <you@example.com>
2019-10-29 19:45:21 +0900

    first commit

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.utils import timezone
 from .models import Post
