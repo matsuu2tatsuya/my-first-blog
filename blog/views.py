7d6194ebfb728f0985b23b056cceaea125643a64
Your Name <you@example.com>
2019-11-12 21:18:13 +0900

    first commit

--- blog/views.py
+++ blog/views.py
@@ -49,7 +49,7 @@ def post_edit(request, pk):
 @login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-    return render(request, 'blog/post_draft_list.html', {'posts':posts})
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 @login_required
 def post_publish(request, pk):
